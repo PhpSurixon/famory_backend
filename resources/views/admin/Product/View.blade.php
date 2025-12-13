a8db0212862df6def35cedbacb92183098b5a465
PhpSurixon <phpsurixon@gmail.com>
2025-12-13 18:34:03 +0530

    upload ocde

--- resources/views/admin/Product/View.blade.php
+++ resources/views/admin/Product/View.blade.php
@@ -160,6 +160,7 @@
                                         <td>
                                             @php
                                             $s3BaseUrl = 'https://famorys3.s3.amazonaws.com';
+                                            $imagpth = str_contains($data->image, $s3BaseUrl) ? $data->image : $s3BaseUrl . '/' . ltrim($data->image, '/');
                                             @endphp
                                             @if(!empty($data->image))
                                                 <img src="{{ str_contains($data->image, $s3BaseUrl) ? $data->image : $s3BaseUrl . '/' . ltrim($data->image, '/') }}" alt="Tag Image" class="img-circle" loading="lazy"onerror="this.src='{{ asset("assets/img/default_tag.jpg") }}'">
@@ -167,7 +168,7 @@
                                                 <img src="{{ asset('assets/img/default_tag.jpg') }}" alt="Default Image" class="img-circle" loading="lazy"onerror="this.src='{{ asset("assets/img/default_tag.jpg") }}'">
                                             @endif
                                         </td>
-                                        <td>{{ $data->name }} {{ $data->image }}</td>
+                                        <td>{{ $data->name }} {{ $imagpth }}</td>
                                         <td>${{ $data->reseller_price ? number_format($data->reseller_price, 2) : 'N/A' }}</td>
                                         <td>${{ $data->price ? number_format($data->price, 2) : 'N/A'}}</td>
                                         <td>{{ $data->count ?? '-'}}</td>
