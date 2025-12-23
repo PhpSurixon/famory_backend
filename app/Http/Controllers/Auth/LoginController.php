19af3bb19dca38d88de5ec574b61bc5e212ac962
PhpSurixon <phpsurixon@gmail.com>
2025-12-23 20:57:55 +0530

    upload code

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -112,6 +112,7 @@ class LoginController extends Controller
         
         if($roleId == 1 || $roleId == 4)
         {
+            dd(1);
             return redirect()->route('admin.login');
         }
         return redirect()->route('login');
