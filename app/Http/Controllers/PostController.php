523522f381d1a8f0bcdc894b6cdab0707b6a35c3
PhpSurixon <phpsurixon@gmail.com>
2025-12-26 14:40:19 +0530

    uplod code of api post feedback chek

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -779,6 +779,7 @@ class PostController extends Controller
                 $post->save();
 
                 // Scheduling
+                dd($timezone);
                 $scheduledDateTime = Carbon::parse($request->schedule_date . ' ' . $request->schedule_time, $timezone)
                                           ->setTimezone('UTC');
 
