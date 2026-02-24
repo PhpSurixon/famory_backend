be3690ca78eb8392a647275637c65c1e759ac9c7
PhpSurixon <phpsurixon@gmail.com>
2026-02-24 23:11:26 +0530

    upload code of api get profile api

--- app/Http/Controllers/Api/TrustedUserController.php
+++ app/Http/Controllers/Api/TrustedUserController.php
@@ -400,7 +400,7 @@ class TrustedUserController extends Controller
             }
 
             $update_notification = Notification::where('item_id',$trusted->id)
-                                                ->where('receiver_id',$trusted->user_id)
+                                                ->where('receiver_id',$trusted->trusted_user_id)
                                                 ->where('has_actioned',0)
                                                 ->where('type','trust_request')
                                                 ->first();
